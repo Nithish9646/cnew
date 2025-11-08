82b23a44c291a6b9ced1eb4bffffc61bbdfc74de
Nithish9646 <nithishp9646@gmail.com>
2025-11-08 20:23:50 +0530

    Update modify.c

--- modify.c
+++ modify.c
@@ -76,92 +76,92 @@ void modify_record(struct st **ptr)
 			}
 			temp=temp->next;
 		}
-             if(num>1)
-	     {
-		char c;
-		printf("* * * * * * * * * * * * * * * * * * * * *\n");
-		printf("*                                       *\n");
-		printf("*      R/r : To Search Rollno           *\n");
-		printf("*      P/p : Percentage Based           *\n");
-		printf("*                                       *\n");
-		printf("* * * * * * * * * * * * * * * * * * * * *\n\n");
-		printf("ENTER YOUR CHOICE : ");
-		scanf(" %c",&c);
-		if((c=='r')||(c=='R'))
+		if(num>1)
 		{
-			printf("ENTER THE ROLL NO \n");
-			scanf("%d",&rl);
-			while(temp1!=0)
+			char c;
+			printf("* * * * * * * * * * * * * * * * * * * * *\n");
+			printf("*                                       *\n");
+			printf("*      R/r : To Search Rollno           *\n");
+			printf("*      P/p : Percentage Based           *\n");
+			printf("*                                       *\n");
+			printf("* * * * * * * * * * * * * * * * * * * * *\n\n");
+			printf("ENTER YOUR CHOICE : ");
+			scanf(" %c",&c);
+			if((c=='r')||(c=='R'))
 			{
-				if(temp1->roll==rl)
+				printf("ENTER THE ROLL NO \n");
+				scanf("%d",&rl);
+				while(temp1!=0)
 				{
-					char op;
-					printf(" __________________________\n");
-					printf("|                          |\n");
-					printf("|   N/n : To Search Name   |\n");
-					printf("|   P/p : Percentage Based |\n");
-					printf("|__________________________|\n\n");
-					printf("ENTER YOUR CHOICE : ");
-					scanf(" %c",&op);
-					if((op=='N')||(op=='n'))
+					if(temp1->roll==rl)
 					{
-						printf("ENTER THE NAME TO MODIFY : ");
-						scanf("%s",newname);
-						strcpy(temp1->name,newname);
-						printf("NAME MODIFED SUCCESSFULLY\n\n");
-						show(*ptr);
+						char op;
+						printf(" __________________________\n");
+						printf("|                          |\n");
+						printf("|   N/n : To Search Name   |\n");
+						printf("|   P/p : Percentage Based |\n");
+						printf("|__________________________|\n\n");
+						printf("ENTER YOUR CHOICE : ");
+						scanf(" %c",&op);
+						if((op=='N')||(op=='n'))
+						{
+							printf("ENTER THE NAME TO MODIFY : ");
+							scanf("%s",newname);
+							strcpy(temp1->name,newname);
+							printf("NAME MODIFED SUCCESSFULLY\n\n");
+							show(*ptr);
+						}
+						else if((op=='P')||(op=='p'))
+						{
+							printf("ENTER THE MARK TO MODIFY : ");
+							scanf("%f",&newmark);
+							temp1->mark=newmark;
+							printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
+							show(*ptr);
+						}
 					}
-					else if((op=='P')||(op=='p'))
-					{
-						printf("ENTER THE MARK TO MODIFY : ");
-						scanf("%f",&newmark);
-						temp1->mark=newmark;
-						printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
-						show(*ptr);
-					}
+					temp1=temp1->next;
 				}
-				temp1=temp1->next;
+			}
+			else if((c=='p')||(c=='P'))
+			{
+				printf("ENTER THE MARK : ");
+				scanf("%f",&mark);
+				while(temp1!=0)
+				{
+					if(temp1->roll==mark)
+					{
+						char op;
+						printf(" __________________________\n");
+						printf("|                          |\n");
+						printf("|   N/n : To Search Name   |\n");
+						printf("|   P/p : Percentage Based |\n");
+						printf("|__________________________|\n\n");
+						printf("ENTER YOUR CHOICE : ");
+						scanf(" %c",&op);
+						if((op=='N')||(op=='n'))
+						{
+							printf("ENTER THE NAME TO MODIFY : ");
+							scanf("%s",newname);
+							strcpy(temp1->name,newname);
+							printf("NAME MODIFED SUCCESSFULLY\n\n");
+							show(*ptr);
+						}
+						else if((op=='P')||(op=='p'))
+						{
+							printf("ENTER THE MARK TO MODIFY : ");
+							scanf("%f",&newmark);
+							temp1->mark=newmark;
+							printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
+							show(*ptr);
+						}
+					}
+					temp1=temp1->next;
+				}
 			}
 		}
-		else if((c=='p')||(c=='P'))
+		else if(num==1)
 		{
-			printf("ENTER THE MARK : ");
-			scanf("%f",&mark);
-			while(temp1!=0)
-			{
-				if(temp1->roll==mark)
-				{
-					char op;
-					printf(" __________________________\n");
-					printf("|                          |\n");
-					printf("|   N/n : To Search Name   |\n");
-					printf("|   P/p : Percentage Based |\n");
-					printf("|__________________________|\n\n");
-					printf("ENTER YOUR CHOICE : ");
-					scanf(" %c",&op);
-					if((op=='N')||(op=='n'))
-					{
-						printf("ENTER THE NAME TO MODIFY : ");
-						scanf("%s",newname);
-						strcpy(temp1->name,newname);
-						printf("NAME MODIFED SUCCESSFULLY\n\n");
-						show(*ptr);
-					}
-					else if((op=='P')||(op=='p'))
-					{
-						printf("ENTER THE MARK TO MODIFY : ");
-						scanf("%f",&newmark);
-						temp1->mark=newmark;
-						printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
-						show(*ptr);
-					}
-				}
-				temp1=temp1->next;
-			}
-		}
-	     }
-	     else if(num==1)
-	     {
 			while(temp1!=0)
 			{
 				if((strcmp(temp1->name,name))==0)
@@ -191,75 +191,115 @@ void modify_record(struct st **ptr)
 						show(*ptr);
 					}
 				}
-	           }
-         	}
+				temp1=temp1->next;
+			}
+		}
 	}
 
 	else if((enter=='P')||(enter=='p'))
 	{
 		printf("ENTER THE MARK : ");
 		scanf("%f",&mark);
-		printf("HERE THE REPEATED MARKS \n");
+		printf("ROLL.NO\tNAMES\tMARKS\n");
+		int num=0;
 		while(temp!=0)
 		{
 			if(temp->mark==mark)
 			{
-				printf("%d %s %f\n",temp->roll,temp->name,temp->mark);
+				num++;
+				printf("%d\t%s\t%f\n",temp->roll,temp->name,temp->mark);
 			}
 			temp=temp->next;
 		}
-		char c;
-		printf("* * * * * * * * * * * * * * * * * * * * *\n");
-		printf("*                                       *\n");
-		printf("*      R/r : To Search Roll             *\n");
-		printf("*      P/p : To Search Name             *\n");
-		printf("*                                       *\n");
-		printf("* * * * * * * * * * * * * * * * * * * * *\n\n");
-		printf("ENTER YOUR CHOICE : ");
-		scanf(" %c",&c);
-		if((c=='r')||(c=='R'))
+		if(num>1)
 		{
-			printf("ENTER THE ROLL NO \n");
-			scanf("%d",&rl);
-			while(temp1!=0)
+			char c;
+			printf("* * * * * * * * * * * * * * * * * * * * *\n");
+			printf("*                                       *\n");
+			printf("*      R/r : To Search Roll             *\n");
+			printf("*      P/p : To Search Name             *\n");
+			printf("*                                       *\n");
+			printf("* * * * * * * * * * * * * * * * * * * * *\n\n");
+			printf("ENTER YOUR CHOICE : ");
+			scanf(" %c",&c);
+			if((c=='r')||(c=='R'))
 			{
-				if(temp1->roll==rl)
+				printf("ENTER THE ROLL NO \n");
+				scanf("%d",&rl);
+				while(temp1!=0)
 				{
-					char op;
-					printf(" __________________________\n");
-					printf("|                          |\n");
-					printf("|   N/n : To Search Name   |\n");
-					printf("|   P/p : Percentage Based |\n");
-					printf("|__________________________|\n\n");
-					printf("ENTER YOUR CHOICE : ");
-					scanf(" %c",&op);
-					if((op=='N')||(op=='n'))
+					if(temp1->roll==rl)
 					{
-						printf("ENTER THE NAME TO MODIFY : ");
-						scanf("%s",newname);
-						strcpy(temp1->name,newname);
-						printf("NAME MODIFED SUCCESSFULLY\n\n");
-						show(*ptr);
+						char op;
+						printf(" __________________________\n");
+						printf("|                          |\n");
+						printf("|   N/n : To Search Name   |\n");
+						printf("|   P/p : Percentage Based |\n");
+						printf("|__________________________|\n\n");
+						printf("ENTER YOUR CHOICE : ");
+						scanf(" %c",&op);
+						if((op=='N')||(op=='n'))
+						{
+							printf("ENTER THE NAME TO MODIFY : ");
+							scanf("%s",newname);
+							strcpy(temp1->name,newname);
+							printf("NAME MODIFED SUCCESSFULLY\n\n");
+							show(*ptr);
+						}
+						else if((op=='P')||(op=='p'))
+						{
+							printf("ENTER THE MARK TO MODIFY : ");
+							scanf("%f",&newmark);
+							temp1->mark=newmark;
+							printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
+							show(*ptr);
+						}
 					}
-					else if((op=='P')||(op=='p'))
-					{
-						printf("ENTER THE MARK TO MODIFY : ");
-						scanf("%f",&newmark);
-						temp1->mark=newmark;
-						printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
-						show(*ptr);
-					}
+					temp1=temp1->next;
 				}
-				temp1=temp1->next;
 			}
-		  }
-		else if((c=='p')||(c=='P'))
+			else if((c=='p')||(c=='P'))
+			{
+				printf("ENTER THE NAME : ");
+				scanf("%s",name);
+				while(temp1!=0)
+				{
+					if((strcmp(temp1->name,name))==0)
+					{
+						char op;
+						printf(" __________________________\n");
+						printf("|                          |\n");
+						printf("|   N/n : To Search Name   |\n");
+						printf("|   P/p : Percentage Based |\n");
+						printf("|__________________________|\n\n");
+						printf("ENTER YOUR CHOICE : ");
+						scanf(" %c",&op);
+						if((op=='N')||(op=='n'))
+						{
+							printf("ENTER THE NAME TO MODIFY : ");
+							scanf("%s",newname);
+							strcpy(temp1->name,newname);
+							printf("NAME MODIFED SUCCESSFULLY\n\n");
+							show(*ptr);
+						}
+						else if((op=='P')||(op=='p'))
+						{
+							printf("ENTER THE MARK TO MODIFY : ");
+							scanf("%f",&newmark);
+							temp1->mark=newmark;
+							printf("MARK WAS SUCCESSFULLY MODIFIED\n\n");
+							show(*ptr);
+						}
+					}
+					temp1=temp1->next;
+				}
+			}
+		}
+		else if(num==1)
 		{
-			printf("ENTER THE NAME : ");
-			scanf("%s",name);
 			while(temp1!=0)
 			{
-				if((strcmp(temp1->name,name))==0)
+				if(temp1->mark==mark)
 				{
 					char op;
 					printf(" __________________________\n");
@@ -289,6 +329,6 @@ void modify_record(struct st **ptr)
 				temp1=temp1->next;
 			}
 		}
-		
+
 	}
 }
